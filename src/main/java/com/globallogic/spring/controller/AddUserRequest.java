2dc67698fb98934015d8c2cbaf09f4980d75e1a9
Oleksandr <avpavlov96@gmail.com>
2016-05-31 17:52:26 +0300

    class AddUserRequest

---
+++ src/main/java/com/globallogic/spring/controller/AddUserRequest.java
@@ -0,0 +1,7 @@
+package com.globallogic.spring.controller;
+
+/**
+ * Created by ALEX on 31.05.2016.
+ */
+public class AddUserRequest {
+}
--- src/main/java/com/globallogic/spring/controller/UserController.java
+++ src/main/java/com/globallogic/spring/controller/UserController.java
@@ -3,6 +3,7 @@ package com.globallogic.spring.controller;
 import com.globallogic.spring.entity.User;
 import com.globallogic.spring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +26,10 @@ public class UserController {
     @RequestMapping(method = RequestMethod.GET)
     public List<User> findAllUsers(){
         return userRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void addUser(@RequestBody AddUserRequest addUserRequest){
+
     }
 }
