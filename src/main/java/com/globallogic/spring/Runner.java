b2872199d5d7ce936d9aa77db99297639c4ca063
Oleksandr <avpavlov96@gmail.com>
2016-05-31 17:24:56 +0300

    class User

--- src/main/java/com/globallogic/spring/Runner.java
+++ src/main/java/com/globallogic/spring/Runner.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootRestApplication {
+public class Runner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootRestApplication.class, args);
+		SpringApplication.run(Runner.class, args);
 	}
 }
---
+++ src/main/java/com/globallogic/spring/entity/User.java
@@ -0,0 +1,18 @@
+package com.globallogic.spring.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by ALEX on 31.05.2016.
+ */
+
+@Entity
+public class User {
+
+    private long id;
+
+    private String name;
+
+    private String surname;
+
+}
