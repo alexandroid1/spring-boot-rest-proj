1d3ee7c2e3f9bc1fa13db3bfe74ad78e7a72f459
Oleksandr <avpavlov96@gmail.com>
2016-05-31 17:48:09 +0300

    RequestMapping

--- src/main/java/com/globallogic/spring/controller/UserController.java
+++ src/main/java/com/globallogic/spring/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.globallogic.spring.controller;
 
+import com.globallogic.spring.entity.User;
 import com.globallogic.spring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by ALEX on 31.05.2016.
@@ -15,5 +20,10 @@ public class UserController {
     @Autowired
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
     }
 }
