4730155d0a449066b64b8aadebf13eef22b899b5
Oleksandr <avpavlov96@gmail.com>
2016-05-31 17:28:08 +0300

    User changes

--- src/main/java/com/globallogic/spring/entity/User.java
+++ src/main/java/com/globallogic/spring/entity/User.java
@@ -1,6 +1,8 @@
 package com.globallogic.spring.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 /**
  * Created by ALEX on 31.05.2016.
@@ -9,10 +11,35 @@ import javax.persistence.Entity;
 @Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private long id;
 
     private String name;
 
     private String surname;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
